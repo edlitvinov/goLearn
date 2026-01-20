996ba1e242739bbd8c597c2c9de4f583e589ec71
Eduard Litvinov <edoss@list.ru>
2026-01-20 13:15:05 +0300

    add 02/main.go

--- 02. scan/main.go
+++ 02. scan/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+	var (
+		age  int
+		name string
+	)
+
+	fmt.Printf("What is you name?\n")
+	fmt.Scan(&name)
+	fmt.Printf("How old are you?\n")
+	fmt.Scan(&age)
+
+	fmt.Printf("You'r Name is: %s\nYou'r Age is: %d", name, age)
 
 }
