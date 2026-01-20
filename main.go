9476b585d161230af7fabd27f6f6d3472242625c
Eduard Litvinov <edoss@list.ru>
2026-01-20 04:59:02 +0300

    create main.go in package main

---
+++ main.go
@@ -0,0 +1,7 @@
+package main //	create package
+
+import "fmt" //	import standart library
+
+func main() { // pointer in
+	fmt.Println("Get Start Learn Golang!") // Println automatic the end string "\n"
+}
