5182dd9aa34eb604a3deceeb33036ae9a4d8d113
Eduard Litvinov <edoss@list.ru>
2026-01-20 10:32:44 +0300

    add variables

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main //	create package
 import "fmt" //	import standart library
 
 func main() { // pointer in
-	fmt.Println("Get Start Learn Golang!") // Println automatic the end string "\n"
+
+	// Println automatic the end string "\n"
+	fmt.Println("Get Start Learn Golang!")
+
+	// Print не осуществляет перевод на новую строку
+	fmt.Print("Fisst")
+	fmt.Print("Second")
+	fmt.Print("Third\n")
+
+	// Printf - форматированный вывод
+	a := "Let's go"
+	b := "learn"
+	c := "Golang"
+
+	fmt.Printf("%s %s %s!\n", a, b, c)
+
 }
