144c1f0e4bc0f0eef5b24fc09201339b26ed1057
Eduard Litvinov <edoss@list.ru>
2026-01-21 13:27:37 +0300

    added if/else

--- 04.flow control/main.go
+++ 04.flow control/main.go
@@ -5,9 +5,28 @@ import "fmt"
 func main() {
 	var value int
 
+	fmt.Println("input any number")
 	fmt.Scan(&value)
 
 	if value%2 == 0 {
-		fmt.Println("The nuymber", value "is even")
+		fmt.Println("The nuymber", value, "is even")
+	} else {
+		fmt.Println("The nuymber", value, "is odd")
 	}
+
+	// else-if
+	// if cond1 {
+	// 	...
+	// } else if cond2 {
+	// 	...
+	// } else if cond3 {
+	// 	...
+	// } else if cond4 {
+	// 	...
+	// } else if ... {
+	// 	...
+	// } else {
+	// 	...
+	// }
+
 }
