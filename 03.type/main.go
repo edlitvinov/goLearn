276fb02a49476f2c4c1e74234291e22ed5ee156e
Eduard Litvinov <edoss@list.ru>
2026-01-21 09:53:48 +0300

    add Bolean type

--- 03.type/main.go
+++ 03.type/main.go
@@ -4,5 +4,17 @@ import "fmt"
 
 func main() {
 	// Boolian
+	var firstBool bool // default false
+
+	fmt.Printf("variable \"firstBool\" is value: %t\n", firstBool)
+	fmt.Printf("variable \"firstBool\" is type: %T\n", firstBool)
+
+	aBool, bBool := true, false
+	fmt.Printf("variable \"aBool\" is value: %t\n", aBool)
+	fmt.Printf("variable \"bBool\" is value: %t\n", bBool)
+	fmt.Printf("aBool && (AND) bBool is: %t\n", aBool && bBool)
+	fmt.Printf("aBool || (OR) bBool is: %t\n", aBool || bBool)
+	fmt.Printf("!(NOT) aBool is: %t\n", !aBool)
+	fmt.Printf("!(NOT) bBool is: %t\n", !bBool)
 
 }
