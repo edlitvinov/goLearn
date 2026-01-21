2c456234cb5f5443a710b158188274dfc4f06c43
Eduard Litvinov <edoss@list.ru>
2026-01-21 10:49:40 +0300

    add Numerics.Integer

--- 03.type/main.go
+++ 03.type/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	// Boolian
+	fmt.Println("*Type Boolian*")
+	fmt.Println("--------------")
 	var firstBool bool // default false
 
 	fmt.Printf("variable \"firstBool\" is value: %t\n", firstBool)
@@ -16,5 +21,28 @@ func main() {
 	fmt.Printf("aBool || (OR) bBool is: %t\n", aBool || bBool)
 	fmt.Printf("!(NOT) aBool is: %t\n", !aBool)
 	fmt.Printf("!(NOT) bBool is: %t\n", !bBool)
+	fmt.Println("------------------------------------------")
+	// Numerics
+	// Int
+	fmt.Println("*Type Integer*")
+	fmt.Println("--------------")
+	fmt.Printf("Integers type (signed): \n- int\n- int8\n- int16\n- int32\n- int64\n")
+	fmt.Printf("Integers type (unsigned): \n- uint\n- uint8\n- uint16\n- uint32\n- uint64\n")
+
+	var a int
+	var b int8
+	var c int16
+	var d int32
+	var e int64
+
+	fmt.Printf("Sizeof variable \"int\" type is: %d bytes\n", unsafe.Sizeof(a))
+	fmt.Printf("Sizeof variable \"int8\" type is: %d bytes\n", unsafe.Sizeof(b))
+	fmt.Printf("Sizeof variable \"int16\" type is: %d bytes\n", unsafe.Sizeof(c))
+	fmt.Printf("Sizeof variable \"int32\" type is: %d bytes\n", unsafe.Sizeof(d))
+	fmt.Printf("Sizeof variable \"int64\" type is: %d bytes\n", unsafe.Sizeof(e))
+	fmt.Println("------------------------------------------")
+	fmt.Printf("Sizeof int == int64 == 8 bytes\n")
+	fmt.Printf("But type int !== type int64\n")
+	fmt.Println("Sizeof signed int == sizeof unsigned int")
 
 }
