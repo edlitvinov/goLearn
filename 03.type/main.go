3a599b4cd847d3d40010f7c12b658b939af096a6
Eduard Litvinov <edoss@list.ru>
2026-01-21 12:11:32 +0300

    added Type Strings

--- 03.type/main.go
+++ 03.type/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"unicode/utf8"
 	"unsafe"
 )
 
@@ -51,4 +53,30 @@ func main() {
 	fmt.Println("------------")
 	fmt.Printf("Float Type in Golang is:\n- float32\n- float64\n")
 
+	// Strings
+	fmt.Println("*Type Strings*")
+	fmt.Println("------------")
+	name := "Tom"
+	nameRu := "Том"
+	lastName := "Sawyer"
+	fullName := name + " " + lastName
+	fmt.Println("My name:", fullName)
+	fmt.Println("Length of string:", name, len(name))
+	fmt.Println("Length of string:", nameRu, len(nameRu))
+	fmt.Println("Rune - 1  \"utf\" symbol")
+	fmt.Println("*Used Rune*")
+	fmt.Println("Length of string:", name, len(name))
+	fmt.Println("Length of string:", nameRu, utf8.RuneCountInString(nameRu))
+	fmt.Println("--------------------")
+	totalString, subString := "Tom Sawyer", "om"
+	fmt.Println(strings.Contains(totalString, subString))
+	fmt.Println("Rune -> alias on type INT32")
+	var firstRune rune
+	var secondRune rune = 'F'
+	var thirdRune rune = 47
+	fmt.Printf("value variable firstRune: %c\n", firstRune)
+	fmt.Printf("value variable secondRune: %c\n", secondRune)
+	fmt.Printf("value variable thirdRune: %c\n", thirdRune)
+	fmt.Printf("Sizeof Rune: %d\n", unsafe.Sizeof(firstRune))
+	fmt.Println("Type BYTE -> alias type UINT8")
 }
