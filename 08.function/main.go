d3a9850cd54b7563be35aeadd08cf4b2a11b3758
Eduard Litvinov <edoss@list.ru>
2026-01-25 08:41:27 +0300

    added function parametrs

--- 08.function/main.go
+++ 08.function/main.go
@@ -3,15 +3,29 @@ package main
 import "fmt"
 
 // Определение простой функции
-func printPrice() {
-	kayakPrice := 275.00
-	kayakTax := kayakPrice * 0.2
-	fmt.Println("Price: ", kayakPrice)
-	fmt.Println("Tax: ", kayakTax)
+// func printPrice() {
+// 	kayakPrice := 275.00
+// 	kayakTax := kayakPrice * 0.2
+// 	fmt.Println("Price: ", kayakPrice)
+// 	fmt.Println("Tax: ", kayakTax)
 
+// Определение и использование параметров функции
+func printPrice(product string, price, taxRate float64) {
+	taxAmount := price * taxRate
+	fmt.Printf("%s\nprice: %.2f\ntax: %.2f\n", product, price, taxAmount)
+	fmt.Println("---------------")
 }
+
 func main() {
-	fmt.Println("About to coll function")
-	printPrice()
-	fmt.Println("Function complit")
+	// Вызов функции printPrice()
+	// fmt.Println("About to coll function")
+	// printPrice()
+	// fmt.Println("Function complit")
+
+	// Вызов функции printPrice() с аргументами
+
+	printPrice("Kayak", 350, 0.5)
+	printPrice("LifeJacket", 37.85, 0.3)
+	printPrice("Soccer Ball", 17.50, 0.1)
+
 }
